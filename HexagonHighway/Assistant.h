60e8e900edbb1be29615b5eb9cfee1408cac7954
MANgolmx <misammmmm@inbox.ru>
2022-01-28 17:03:16 +0300

    Удаление не нужной функции

---
+++ HexagonHighway/Assistant.h
@@ -0,0 +1,19 @@
+#include "Cell.h"
+#include "DrivableCell.h"
+#include "NonDrivableCell.h"
+
+bool isBelong(Vector2i a, DrivableCell cell);
+
+void SetPath(DrivableCell* mas, const int N,
+	int straight, int turned, int threeway, int fourway);
+
+void CheckSwap(const int N, DrivableCell cell[], RenderWindow& window);
+
+void ReadMainPositions(const int N, DrivableCell cell[]);
+
+void SetPositions(const int N, DrivableCell cell[]);
+
+void SetIsChosen(bool var, const int N, DrivableCell[]);
+
+void DrawCells(RenderWindow& win, DrivableCell cell[], NonDrivableCell chosen, 
+	const int N);
