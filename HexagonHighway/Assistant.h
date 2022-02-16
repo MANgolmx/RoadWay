07ddf3a0827cefba44e4450f269d3407ca29add5
MANgolmx <misammmmm@inbox.ru>
2022-02-16 17:02:25 +0300

    Рандомная генерация новых декораций

--- HexagonHighway/Assistant.h
+++ HexagonHighway/Assistant.h
@@ -1,8 +1,13 @@
+#pragma once
+
 #include "Cell.h"
 #include "DrivableCell.h"
 #include "NonDrivableCell.h"
+#include "Button.h"
 
 bool isBelong(Vector2i a, DrivableCell cell);
+
+bool isBelong(Vector2i a, Button bt);
 
 void SetDrivablePath(DrivableCell* mas, const int masSize,
 	int straight, int turned, int threeway, int fourway);
@@ -20,5 +25,5 @@ void SetPositions(const int roadSize, DrivableCell cell[],
 
 void SetIsChosen(bool var, const int N, DrivableCell cell[]);
 
-void DrawCells(RenderWindow& win, DrivableCell roads[], NonDrivableCell chosen, 
+void DrawCells(RenderWindow& win, DrivableCell roads[], NonDrivableCell chosen,
 	const int roadSize, NonDrivableCell decor[], const int decorSize);
