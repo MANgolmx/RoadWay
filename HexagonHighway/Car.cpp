35137759305ab0a013ba6ae45e5ccd5172e10eda
MANgolmx <misammmmm@inbox.ru>
2022-03-26 12:56:04 +0300

    Исправлен баг с проездом сквозь клетку

--- HexagonHighway/Car.cpp
+++ HexagonHighway/Car.cpp
@@ -42,7 +42,6 @@ Car::Car(std::string path)
 void Car::Move(Time time, DrivableCell roads[], const int roadCount,
 	NonDrivableCell decor[], const int decorCount)
 {
-	//TODO: Fix turned cell problem
 	if (isMoving)
 	if (timePassed + time.asMilliseconds() > timeToMove)
 	{
