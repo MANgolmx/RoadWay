4bcbe6a1f1d66e377faf092f0267f5c4c98208a8
MANgolmx <misammmmm@inbox.ru>
2022-03-27 15:17:07 +0300

    Добавление подписи в настройки

--- HexagonHighway/Settings.cpp
+++ HexagonHighway/Settings.cpp
@@ -29,7 +29,7 @@ int settings(sf::RenderWindow& window, float &volume)
 	FText txt_volume_minus(" ", 0, "resources\\fonts\\pixeltime\\PixelTimes.ttf");
 	Button bt_volume_minus(txt_volume_minus, "resources\\buttons\\button_volume.png", { 0,0 });
 
-	f1 = window.getSize().x / 2 - bt_volume_minus.GetTexture().getSize().x / 2 - 100;
+	f1 = window.getSize().x / 2 - bt_volume_minus.GetTexture().getSize().x / 2 + 200;
 	f2 = window.getSize().y / 4 - bt_volume_minus.GetTexture().getSize().y;
 	bt_volume_minus.SetPosition({ f1,f2 });
 
@@ -38,23 +38,29 @@ int settings(sf::RenderWindow& window, float &volume)
 	FText txt_volume_bg(" ", 0, "resources\\fonts\\pixeltime\\PixelTimes.ttf");
 	Button bt_volume_bg(txt_volume_bg, "resources\\buttons\\button_settings.png", { 0,0 });
 
-	f1 = window.getSize().x / 2 - bt_volume_bg.GetTexture().getSize().x / 2 - 62;
+	f1 = window.getSize().x / 2 - bt_volume_bg.GetTexture().getSize().x / 2 + 238;
 	f2 = window.getSize().y / 4 - bt_volume_bg.GetTexture().getSize().y + 16;
 	bt_volume_bg.SetPosition({ f1,f2 });
 
 	FText txt_volume_plus(" ", 0, "resources\\fonts\\pixeltime\\PixelTimes.ttf");
 	Button bt_volume_plus(txt_volume_plus, "resources\\buttons\\button_volume.png", { 0,0 });
 
-	f1 = window.getSize().x / 2 - bt_volume_plus.GetTexture().getSize().x / 2 + 100;
+	f1 = window.getSize().x / 2 - bt_volume_plus.GetTexture().getSize().x / 2 + 400;
 	f2 = window.getSize().y / 4 - bt_volume_plus.GetTexture().getSize().y;
 	bt_volume_plus.SetPosition({ f1,f2 });
 
 	std::string vol = std::to_string(int(volume));
 	FText txt_vol(vol, 80, "resources\\fonts\\pixeltime\\PixelTimes.ttf");
 	
-	f1 = window.getSize().x / 2 - 106;
+	f1 = window.getSize().x / 2 + 194;
 	f2 = window.getSize().y / 4 - bt_volume_plus.GetTexture().getSize().y + 8;
 	txt_vol.SetPosition({ f1,f2 });
+
+	FText txt_volume("VOLUME", 80, "resources\\fonts\\pixeltime\\PixelTimes.ttf");
+
+	f1 = window.getSize().x / 2 - 506;
+	f2 = window.getSize().y / 4 - bt_volume_plus.GetTexture().getSize().y + 8;
+	txt_volume.SetPosition({ f1,f2 });
 
 	window.setFramerateLimit(60);
 
@@ -95,21 +101,25 @@ int settings(sf::RenderWindow& window, float &volume)
 				f2 = (1080 / 2 - window.getSize().y / 2);
 				menu.SetPosition({ -f1,-f2 });
 
-				f1 = window.getSize().x / 2 - bt_volume_plus.GetTexture().getSize().x / 2 + 100;
+				f1 = window.getSize().x / 2 - bt_volume_plus.GetTexture().getSize().x / 2 + 400;
 				f2 = window.getSize().y / 4 - bt_volume_plus.GetTexture().getSize().y;
 				bt_volume_plus.SetPosition({ f1,f2 });
 
-				f1 = window.getSize().x / 2 - bt_volume_minus.GetTexture().getSize().x / 2 - 100;
+				f1 = window.getSize().x / 2 - bt_volume_minus.GetTexture().getSize().x / 2 + 200;
 				f2 = window.getSize().y / 4 - bt_volume_minus.GetTexture().getSize().y;
 				bt_volume_minus.SetPosition({ f1,f2 });
 
-				f1 = window.getSize().x / 2 - 106;
+				f1 = window.getSize().x / 2 + 194;
 				f2 = window.getSize().y / 4 - bt_volume_plus.GetTexture().getSize().y + 8;
 				txt_vol.SetPosition({ f1,f2 });
 
-				f1 = window.getSize().x / 2 - bt_volume_bg.GetTexture().getSize().x / 2 - 62;
+				f1 = window.getSize().x / 2 - bt_volume_bg.GetTexture().getSize().x / 2 + 238;
 				f2 = window.getSize().y / 4 - bt_volume_bg.GetTexture().getSize().y + 16;
 				bt_volume_bg.SetPosition({ f1,f2 });
+
+				f1 = window.getSize().x / 2 - 506;
+				f2 = window.getSize().y / 4 - bt_volume_plus.GetTexture().getSize().y + 8;
+				txt_volume.SetPosition({ f1,f2 });
 				break;
 			}
 		}
@@ -122,6 +132,7 @@ int settings(sf::RenderWindow& window, float &volume)
 		bt_volume_plus.Draw(window);
 		bt_volume_bg.Draw(window);
 		window.draw(txt_vol.GetText());
+		window.draw(txt_volume.GetText());
 		window.display();
 	}
 
